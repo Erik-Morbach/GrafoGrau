7556d911e08a6c85caa9dcf4e0079d637adfc491
Erik Morbach <morbacherik@gmail.com>
2021-06-13 19:17:35 -0300

    configurando nodos e arestas

--- src/model/Edge.java
+++ src/model/Edge.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Edge {
+	private int degree;
+	private Node destiny;
+	
+	public Edge() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Edge(int degree, Node destiny) {
+		super();
+		this.degree = degree;
+		this.destiny = destiny;
+	}
+	public int getDegree() {
+		return degree;
+	}
+	public void setDegree(int degree) {
+		this.degree = degree;
+	}
+	public Node getDestiny() {
+		return destiny;
+	}
+	public void setDestiny(Node destiny) {
+		this.destiny = destiny;
+	}
 
+	
+	
 }
