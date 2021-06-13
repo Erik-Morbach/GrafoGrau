cc186344221e1cd13bc202b8f146a524672b2592
Cassimiroardo <edu27.cassimiro@gmail.com>
2021-06-13 19:11:41 -0300

    renomeando grafo

---
+++ src/model/FamilyGraph.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyGraph {
+	private List<Edge> edges;
+	private List<Node> nodes;
+	
+	public FamilyGraph() {
+		this.edges = new ArrayList<Edge>();
+		this.nodes = new ArrayList<Node>();
+	}
+	
+	private void addNode() {
+		
+	}
+	
+	private void addEdge() {
+		
+	}
+	
+	public void addRelative() {
+		
+	}
+}
