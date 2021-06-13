69b07faae5b1a7428e9049652f4d4a9e36cbae30
Erik Morbach <morbacherik@gmail.com>
2021-06-13 20:40:53 -0300

    Pessoa

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,17 @@
 package model;
 
-public class Person {
+public class Person implements Comparable<Person>{
+	private String name;
+	@Override
+	public int compareTo(Person person) {
+		return this.name.compareTo(person.getName());
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	
 }
